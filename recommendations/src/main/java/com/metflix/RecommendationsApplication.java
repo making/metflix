8e59b55e791e8f4e5c2ac016fd5c90205ce49a25
making <makingx@gmail.com>
2016-04-21 01:36:27 +0900

    Polish
    (cherry picked from commit 61c4fc51baba7d0c90766d03b9a5c8a37fe30aaa)

--- recommendations/src/main/java/com/metflix/RecommendationsApplication.java
+++ recommendations/src/main/java/com/metflix/RecommendationsApplication.java
@@ -21,7 +21,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 @SpringBootApplication
 @EnableDiscoveryClient
